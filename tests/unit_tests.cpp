58cde5cc470075b6174b93d50f17949fb1c888ff
Stanislav Hahin <stanislav.hahin@gmail.com>
2025-11-27 15:03:45 +0200

    ci: replace workflow with matrix build-and-test

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,5 +1,32 @@
-#include <iostream>
 #include "math_operations.h"
+
+#if defined(USE_GTEST)
+#include <gtest/gtest.h>
+
+TEST(AddTest, PositiveNumbers) {
+    EXPECT_EQ(add(2, 3), 5);
+}
+
+TEST(AddTest, NegativeNumbers) {
+    EXPECT_EQ(add(-4, -6), -10);
+}
+
+TEST(AddTest, MixedSign) {
+    EXPECT_EQ(add(-2, 2), 0);
+}
+
+TEST(AddTest, LargeNumbers) {
+    EXPECT_EQ(add(1000000, 2000000), 3000000);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+#else
+
+#include <iostream>
 
 int main() {
     int failures = 0;
@@ -26,3 +53,5 @@ int main() {
 
     return failures == 0 ? 0 : 1;
 }
+
+#endif
